8944c88c1071b1091708946d7108ac71c5f6ae05
jko0531 <jko0531@gmail.com>
2016-11-22 13:30:55 -0500

    added docstrings, removed useless comments, better structure

--- college_preprocessor.py
+++ college_preprocessor.py
@@ -5,6 +5,9 @@ import redis
 import pika
 
 def SAT1(score):
+	'''
+	Splits the SAT score range string into two real valued numbers
+	'''
 	if score != 'None':
 		if '-' not in score:
 			begin = score
@@ -16,6 +19,9 @@ def SAT1(score):
 	return 'None'
 
 def SAT2(score):
+	'''
+	Splits the SAT score range string into two real valued numbers
+	'''
 	if score != 'None':
 		if '-' not in score:
 			end = score
@@ -27,6 +33,9 @@ def SAT2(score):
 	return 'None'
 
 def ACT1(score):
+	'''
+	Splits the ACT score range string into two real valued numbers
+	'''
 	if score != 'None':
 		if '-' not in score:
 			begin = score
@@ -38,6 +47,9 @@ def ACT1(score):
 	return 'None'
 
 def ACT2(score):
+	'''
+	Splits the ACT score range string into two real valued numbers
+	'''
 	if score != 'None':
 		if '-' not in score:
 			end = score
@@ -49,50 +61,89 @@ def ACT2(score):
 	return 'None'
 
 def ratio(rat):
+	'''
+	Converts the student-faculty ratio string into a real valued number
+	'''
 	if rat != 'None':
 		student = rat.split(':')[0]
 		faculty = rat.split(':')[1]
 		return float(student)/float(faculty)
 	return 'None'
 
-# csv file is located on the desktop
-csv_file = 'assets/colleges.csv'
-col_file = 'assets/Column Labels.txt'
+def parselabels(cols_file):
+	'''
+	Parses the features text file into an array and returns it
+	'''
+	columns = []
+	with open(cols_file) as f:
+		for item in f:
+			columns = item.split(',')
 
-columns = []
-with open(col_file) as f:
-	for item in f:
-		columns = item.split(',')
-df = pd.read_csv(csv_file, sep='\t', header=None)
-df.columns = columns
-#print df['SAT/ACT 25th-75th percentile']
-df['SAT1'] = df['SAT/ACT 25th-75th percentile'].apply(SAT1)
-df['SAT2'] = df['SAT/ACT 25th-75th percentile'].apply(SAT2)
-df['ACT1'] = df['SAT/ACT 25th-75th percentile'].apply(ACT1)
-df['ACT2'] = df['SAT/ACT 25th-75th percentile'].apply(ACT2)
-df['Student-faculty ratio'] = df['Student-faculty ratio'].apply(ratio)
-df.drop('SAT/ACT 25th-75th percentile', axis=1, inplace=True)
-df = df.sort_index(axis=1)
-columns.sort()
-df.to_csv('assets/out.csv', sep='\t', index=False)
+	return columns
 
-columnString = ""
-collegeString = ""
-with open('assets/out.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	next(reader, None)
-	for row in reader:
-		collegeString += row[0] + "\n"
-#print collegeString
+def parsecolleges(college_file, columns):
+	'''
+	Parses the colleges csv file into a pandas dataframe and
+	adjusts SAT/ACT & student/faculty ratio to contain
+	real valued numbers for ML algorithms
+	'''
+	df = pd.read_csv(college_file, sep='\t', header=None)
+	df.columns = columns
+	df['SAT1'] = df['SAT/ACT 25th-75th percentile'].apply(SAT1)
+	df['SAT2'] = df['SAT/ACT 25th-75th percentile'].apply(SAT2)
+	df['ACT1'] = df['SAT/ACT 25th-75th percentile'].apply(ACT1)
+	df['ACT2'] = df['SAT/ACT 25th-75th percentile'].apply(ACT2)
+	df['Student-faculty ratio'] = df['Student-faculty ratio'].apply(ratio)
+	df.drop('SAT/ACT 25th-75th percentile', axis=1, inplace=True)
+	df = df.sort_index(axis=1)
+	df.to_csv('assets/out.csv', sep='\t', index=False)
 
-for i in columns:
-	columnString += i + "\tnumerical\n"
-#print columnString
+def getcollegestring():
+	'''
+	Converts the colleges csv file into a string for redis
+	'''
+	college_string = ""
+	with open('assets/out.csv') as csvfile:
+		reader = csv.reader(csvfile)
+		next(reader, None)
+		for row in reader:
+			college_string += row[0] + "\n"
 
-r = redis.StrictRedis(host=os.environ['PG_HOST'])
-r.set('learning:college_training.csv', collegeString)
-r.set('learning:college_features.csv', columnString)
+	return college_string
 
-#os.remove('assets/out.csv')
+def getfeaturestring(columns):
+	'''
+	Converts the columns txt file into a string for redis
+	'''
+	columns.sort()
+	column_string = ""
+	for i in columns:
+		column_string += i + "\tnumerical\n"
 
-#print df2
+	return column_string
+
+def setredis(column_string, college_string):
+	'''
+	Adds the colleges & features string into the redis db
+	'''
+	r = redis.StrictRedis(host=os.environ['PG_HOST'])
+	r.set('learning:college_training.csv', college_string)
+	r.set('learning:college_features.csv', column_string)
+
+def main():
+	'''
+	Preprocesses the colleges data and ultimately adds it into redis
+	'''
+	college_file = 'assets/colleges.csv'
+	cols_file = 'assets/Column Labels.txt'
+	
+	columns = parselabels(cols_file)
+	parsecolleges(college_file, columns)
+	college_string = getcollegestring()
+	column_string = getfeaturestring(columns)
+	setredis(column_string, college_string)
+
+
+
+if __name__ == '__main__':
+	main()
--- test.py
+++ test.py
@@ -5,47 +5,56 @@ import redis
 import pika
 
 def initialize(college_file, cols_file):
-	columnString = ""
-	collegeString = ""
+	'''
+	Converts the txt and csv files into strings in order to compare
+	with the strings in the redis
+	'''
+	column_string = ""
+	college_string = ""
 	with open(college_file) as csvfile:
 		reader = csv.reader(csvfile)
 		next(reader, None)
 		for row in reader:
-			collegeString += row[0] + "\n"
+			college_string += row[0] + "\n"
 
 	with open(cols_file) as txtfile:
 		line = txtfile.readline().strip()
 		line = line.split(',')
 		line.sort()
 		for i in line:
-			columnString += i + "\tnumerical\n"
+			column_string += i + "\tnumerical\n"
 
-	return collegeString, columnString
+	return college_string, column_string
 
-def testFeaturesCSV(column_string, r):
+def testfeaturescsv(column_string, r):
+	'''
+	Compares the created features string with the one in redis
+	'''
 	output_features = r.get('learning:college_features.csv')
-	#output_features_text = output_features.decode('utf-8').rstrip()
-	#print output_features_text
-	#print column_string
 	assert output_features == column_string, 'Error with features.txt' 
 
 
-def testCollegesCSV(college_string, r):
+def testcollegescsv(college_string, r):
+	'''
+	Compares the created colleges string with the one in redis
+	'''
 	output_colleges = r.get('learning:college_training.csv')
-	#output_colleges_text = output_colleges.decode('utf-8').rstrip()
-
 	assert output_colleges == college_string, 'Error with colleges.csv'
 
 
-def Main():
+def main():
+	'''
+	Test file that ensures the strings that were added into redis
+	are complete/correct
+	'''
 	college_file = 'assets/out.csv'
 	cols_file = 'assets/Column Labels.txt'
 	r = redis.StrictRedis(host=os.environ['PG_HOST'])
 	college_string, column_string = initialize(college_file, cols_file)
 
-	testFeaturesCSV(column_string, r)
-	testCollegesCSV(college_string, r)
+	testfeaturescsv(column_string, r)
+	testcollegescsv(college_string, r)
 
 
 if __name__ == '__main__':
-	Main()
+	main()
