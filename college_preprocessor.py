1014fdb0370bb3b4bcdd2d8aed8bdfecb69a76e0
jko0531 <jko0531@gmail.com>
2016-12-17 23:29:26 -0500

    changed for spec requirements

--- college_preprocessor.py
+++ college_preprocessor.py
@@ -100,15 +100,86 @@ def separate_parens(value):
     
     return value
 
+def category_yn(value):
+	'''
+	Cells with yes/no values are changed to 1/0
+	yes = 1
+	no = 0
+	'''
+	if isinstance(value, basestring):
+		if value == 'Yes':
+			return 1
+		if value == 'No':
+			return 0
+	return value
+
+def category_enlist(value):
+	'''
+	Category parsing for army/navy/airforce
+	not offered = 0
+	offered on campus = 1
+	offered at other institute = 2
+	'''
+	if isinstance(value, basestring):
+		if 'Offered on campus' in value:
+			return 1
+		if 'Offered at cooperating institution' in value:
+			return 2
+	return 0
+
+def category_tests(value):
+	'''
+	Category parsing for standardized tests
+	No tests required = 0
+	SAT only = 1
+	ACT only = 2
+	EIther SAT or ACT = 3
+	'''
+	if isinstance(value, basestring):
+		if value == 'Neither SAT nor ACT':
+			return 0
+		if value == 'SAT':
+			return 1
+		if value == 'ACT':
+			return 2
+		if value == 'Either SAT or ACT':
+			return 3
+	return 0
+
+def category_selectivity(value):
+	'''
+	Category parsing for selectivity
+	Least selective = 0
+	Less selective = 1
+	Selective = 2
+	More selective = 3
+	Most selective = 4
+	'''
+	if isinstance(value, basestring):
+		if value == 'Least selective':
+			return 0
+		if value == 'Less selective':
+			return 1
+		if value == 'Selective':
+			return 2
+		if value == 'More selective':
+			return 3
+		if value == 'Most selective':
+			return 4
+	return 0
+
 def parselabels(cols_file):
     '''
-    Parses the features text file into an array and returns it
+    Parses the features csv file into an array and returns it
     '''
     columns = []
-    with open(cols_file) as f:
-        for item in f:
-            columns = item.split(',')
-
+    with open(cols_file, 'a+') as f:
+    	reader = csv.reader(f)
+    	for row in reader:
+    		columns.append(row[0])
+    
+    columns.sort()
+    
     return columns
 
 def parsecolleges(college_file, columns):
@@ -117,32 +188,68 @@ def parsecolleges(college_file, columns):
     adjusts SAT/ACT & student/faculty ratio to contain
     real valued numbers for ML algorithms
     '''
+
+    # read the csv
     df = pd.read_csv(college_file, header=None)
     df.columns = columns
-    df['SAT1'] = df['SAT/ACT 25th-75th percentile'].apply(SAT_begin)
-    df['SAT2'] = df['SAT/ACT 25th-75th percentile'].apply(SAT_end)
-    df['ACT1'] = df['SAT/ACT 25th-75th percentile'].apply(ACT_begin)
-    df['ACT2'] = df['SAT/ACT 25th-75th percentile'].apply(ACT_end)
-    df['Student-faculty ratio'] = df['Student-faculty ratio'].apply(ratio)
+
+    # change all 'N/A' values to 0
+    df.fillna(0, inplace=True)
+
+    # category for yes/no
+    df = df.applymap(category_yn)
+
+    # fix the small issue where extra unecessary info is in cells
+    df = df.applymap(separate_parens)
+
+    # strip cells of $,% to include only numbers or categories
+    df = df.applymap(strip_chars)
+
+    # split the SAT/ACT range into two cells with lower/upper values
+    df['SAT-lower-percentile'] = df['SAT/ACT 25th-75th percentile'].apply(SAT_begin)
+    df['SAT-upper-percentile'] = df['SAT/ACT 25th-75th percentile'].apply(SAT_end)
+    df['ACT-lower-percentile'] = df['SAT/ACT 25th-75th percentile'].apply(ACT_begin)
+    df['ACT-upper-percentile'] = df['SAT/ACT 25th-75th percentile'].apply(ACT_end)
+    df['Student-faculty-ratio'] = df['Student-faculty ratio'].apply(ratio)
+    df['Air-Force-ROTC'] = df['Air-Force-ROTC'].apply(category_enlist)
+    df['Army-ROTC'] = df['Army-ROTC'].apply(category_enlist)
+    df['Navy-ROTC'] = df['Nacy-ROTC'].apply(category_enlist)
+    df['Required-standardized-tests'] = df['Required-standardized-tests'].apply(category_tests)
+    df['Selectivity'] = df['Selectivity'].apply(category_selectivity)
     df.drop('SAT/ACT 25th-75th percentile', axis=1, inplace=True)
     df = df.sort_index(axis=1)
 
     csv_file_obj = StringIO.StringIO()
 
-    df.to_csv(csv_file_obj, sep='\t', index=False)
+    df.to_csv(csv_file_obj, index=False)
 
     csv_file_obj.seek(0)
     return csv_file_obj
 
-def getfeaturestring(columns):
+def getfeaturestring(cols_file):
     '''
     Converts the columns txt file into a string for redis
     '''
-    columns.sort()
-    column_string = ""
-    for i in columns:
-        column_string += i + "\tnumerical\n"
+    feature_list = []
+    with open(cols_file, 'a+') as f:
+    	reader = csv.reader(f)
+    	for row in reader:
+    		feature_list.append(row)
 
+    # add the new columns we created from SAT/ACT range
+    feature_list.append(['SAT-lower-percentile', 'numerical'])
+    feature_list.append(['SAT-upper-percentile', 'numerical'])
+    feature_list.append(['ACT-lower-percentile', 'numerical'])
+    feature_list.append(['ACT-upper-percentile', 'numerical'])
+    feature_list.remove(['SAT/ACT-25th-75th-percentile', 'ranking'])
+    feature_list.sort()
+    column_string = "attribute_name,type,categories\n"
+
+    for feature in feature_list:
+    	for j in feature:
+        	column_string += j + ','
+        column_string = column_string.strip(',')
+        column_string += '\n'
     return column_string
 
 def setredis(column_string, college_string):
@@ -159,11 +266,10 @@ def rabbitmq_callback(ch, method, properties, body):
     try:
         college_file = 'assets/colleges.csv'
         cols_file = 'assets/column_labels.csv'
-
         columns = parselabels(cols_file)
-        csv_file_obj = parsecolleges(college_file, columns)
+        csv_file_obj = parsecolleges(college_file, columns, id_to_name)
         college_string = csv_file_obj.read()
-        column_string = getfeaturestring(columns)
+        column_string = getfeaturestring(cols_file)
         setredis(column_string, college_string)
 
     except Exception as e:
